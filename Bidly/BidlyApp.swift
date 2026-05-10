020785106cdaf0979627147e5dcc9c6b0c8c1dbc
Vincent Hunter <jhearst29@gmail.com>
2026-05-10 08:45:51 -0400

    Initial Project setup

---
+++ Bidly/BidlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BidlyApp.swift
+//  Bidly
+//
+//  Created by Vincent Hunter on 5/10/26.
+//
+
+import SwiftUI
+
+@main
+struct BidlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
