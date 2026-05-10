918d75bf1faf3e0c5bd400a69d43660801b44bfa
Vincent Hunter <jhearst29@gmail.com>
2026-05-10 09:41:04 -0400

    Created the main Listing.swift file that contains the models for the data that will be used / shown for each item;

---
+++ Bidly/Listing.swift
@@ -0,0 +1,16 @@
+//
+//  Listing.swift
+//  Bidly
+//
+//  Created by Vincent Hunter on 5/10/26.
+//
+
+import Foundation
+
+struct Listing: Identifiable {
+    let id: UUID = UUID()
+    let itemName: String
+    let sellerName: String
+    let currentBid: Double
+    let imageName: String
+}
---
+++ Bidly/ListingCardView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingCardView.swift
+//  Bidly
+//
+//  Created by Vincent Hunter on 5/10/26.
+//
+
+import SwiftUI
+
+struct ListingCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingCardView()
+}
